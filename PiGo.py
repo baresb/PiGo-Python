e44a55ce3330ebf500de6699bd2e22fb630cea79
Bradley G Bares <BradleyBares@Bradleys-MacBook-Air.local>
2015-11-09 10:29:07 -0500

    added dance and edited methods

--- PiGo.py
+++ PiGo.py
@@ -1,19 +1,22 @@
 from gopigo import *
 import time
 
+TOP_DIST = 75
+
 class PiGo:
 
     #####
     ##### BASIC STATUS AND METHODS
     #####
 
-    status = {'ismoving' : False, 'servo' : 90, 'leftspeed' : 175, 'rightspeed' : 175}
+    status = {'ismoving' : False, 'servo' : 90, 'leftspeed' : 175, 'rightspeed' : 175, "dist" : 100}
 
     ismoving = False
     servoPos = 90
 
     def __init__(self):
         print "Hey, What's up, Hello!?!"
+        self.status["dist"] = us_dist(15)
 
     def stop(self):
         self.status["ismoving"] = False
@@ -27,29 +30,50 @@ class PiGo:
         for x in range(3):
             fwd()
 
+    def keepGoing(self):
+        if self.status["dist"] < STOP_DIST:
+            return False
+        else:
+            return True
+
+    def checkDist(self):
+        self.status["dist"] = us_dist(15)
+        print "I see something " + str(self.status["dist"]) + "mm away."
+
     #####
     ##### COMPLEX METHODS
     #####
 
-    #def keyControl(self): #https://github.com/DexterInd/GoPiGo/blob/master/Software/Python/Examples/Basic_Robot_Control/basic_robot.py#
+    def dance(self):
+        print "I just want to DANCE!"
+        self.spin()
+        self.shuffle()
+        self.shakeServo()
+        self.rturn()
+        self.lturn()
+        self.blink()
+
+    def keyControl(self): #https://github.com/DexterInd/GoPiGo/blob/master/Software/Python/Examples/Basic_Robot_Control/basic_robot.py#
         while True:
             print "Enter the Command:",
             a=raw_input()	# Fetch the input from the terminal
-            if a=='w':
-                fwd()	# Move forward
-            elif a=='a':
-                left()	# Turn left
-            elif a=='d':
+            if a == 'w':
+                fwd()  # Move forward
+            elif a == 'a':
+                left()  # Turn left
+            elif a == 'd':
                 right()	# Turn Right
-            elif a=='s':
-                bwd()	# Move back
-            elif a=='q':
-                stop()	# Stop
-            elif a=='z':
-                increase_speed()	# Increase speed
-            elif a=='x':
-                decrease_speed()	# Decrease speed
-            elif a=='e':
+            elif a == 's':
+                bwd()  # Move back
+            elif a == 'q':
+                stop()  # Stop
+            elif a == 'z':
+                increase_speed()  # Increase speed
+            elif a == 'x':
+                decrease_speed()  # Decrease speed
+            elif a == 'r'
+                dance()  # Dance!
+            elif a == 'e':
                 print "Exiting"		# Exit
                 sys.exit()
             else:
@@ -61,6 +85,9 @@ class PiGo:
     #####
 
 piggy = PiGo()
-piggy.fwd()
-piggy.sleep(2)
-piggy.stop()
+
+while piggy.keepGoing():
+    piggy.checkDist()
+    piggy.fwd()
+    piggy.sleep(2)
+    piggy.stop()
